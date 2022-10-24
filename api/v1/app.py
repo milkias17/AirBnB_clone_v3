7de0b5eb5301509f4dbefb18b23e22b53b5fddfc
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2022-10-24 15:51:05 +0300

    handle CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,13 @@
 from os import getenv
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
+CORS(app, resources=r"/*", origins="0.0.0.0")
 
 app.register_blueprint(app_views)
 
